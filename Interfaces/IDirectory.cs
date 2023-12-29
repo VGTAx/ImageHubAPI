94ea1de554af2f616a1e288deaa9400e0a4682a9
Mikhail Novikau <melmenx@gmail.com>
2023-12-29 16:23:44 +0300

    Implement and Add DirectoryService

---
+++ Interfaces/IDirectory.cs
@@ -0,0 +1,22 @@
+﻿namespace ImageHubAPI.Interfaces
+{
+  /// <summary>
+  /// 
+  /// </summary>
+  public interface IDirectory
+  {
+    /// <summary>
+    /// Determines whether the given path refers to an existing directory on disk.
+    /// </summary>
+    /// <param name="path">The path to test</param>
+    /// <returns>true if path refers to an existing directory; false if the directory does not exist or an error occurs when trying to determine if the specified directory exists.</returns>
+    bool Exists(string path);
+
+    /// <summary>
+    /// Creates all the directories in a specified path. 
+    /// </summary>
+    /// <param name="path">The directory to create</param>
+    /// <returns>An object <see cref="DirectoryInfo"/> that represents the directory at the specified path.</returns>
+    DirectoryInfo CreateDirectory(string path);
+  }
+}
---
+++ Service/DirectoryService.cs
@@ -0,0 +1,30 @@
+﻿using ImageHubAPI.Interfaces;
+
+namespace ImageHubAPI.Service
+{
+  /// <summary>
+  /// 
+  /// </summary>
+  public class DirectoryService : IDirectory
+  {
+    /// <summary>
+    /// Creates all the directories in a specified path. 
+    /// </summary>
+    /// <param name="path">The directory to create</param>
+    /// <returns>An object <see cref="DirectoryInfo"/> that represents the directory at the specified path.</returns>
+    public DirectoryInfo CreateDirectory(string path)
+    {
+      return Directory.CreateDirectory(path);
+    }
+
+    /// <summary>
+    /// Determines whether the given path refers to an existing directory on disk.
+    /// </summary>
+    /// <param name="path">The path to test</param>
+    /// <returns>true if path refers to an existing directory; false if the directory does not exist or an error occurs when trying to determine if the specified directory exists.</returns>
+    public bool Exists(string path)
+    {
+      return Directory.Exists(path);
+    }
+  }
+}
