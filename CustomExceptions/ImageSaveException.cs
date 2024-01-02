175e84ff8b8f445f36f49733adb2a6fd8c15a53b
Mikhail Novikov <melmenx@gmail.com>
2024-01-02 18:59:49 +0300

    add unit test

---
+++ CustomExceptions/ImageSaveException.cs
@@ -0,0 +1,26 @@
+﻿namespace ImageHubAPI.CustomExceptions
+{
+  /// <summary>
+  /// 
+  /// </summary>
+  public class ImageSaveException : Exception
+  {
+    /// <summary>
+    /// 
+    /// </summary>
+    public ImageSaveException() : base() { }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="message"></param>
+    public ImageSaveException(string message) : base(message) { }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="message"></param>
+    /// <param name="innerException"></param>
+    public ImageSaveException(string message, Exception innerException) : base(message, innerException) { }
+  }
+}
